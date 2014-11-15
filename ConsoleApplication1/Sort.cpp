305a526ff83619f671a7930eaea3620daa011fbe
Rubytox <c.fag@hotmail.fr>
2014-11-15 17:16:31 +0100

    Ajout Sort.cpp

---
+++ ConsoleApplication1/Sort.cpp
@@ -0,0 +1,56 @@
+#include "Sort.hpp"
+
+using namespace std;
+
+Sort::Sort(std::string nom, int puissance, int niveauRequis) : m_nom(nom), m_puissance(puissance), m_niveauRequis(niveauRequis), m_ID(-1)
+{
+
+}
+
+Sort::~Sort()
+{}
+
+int Sort::getID() const
+{
+	return m_ID;
+}
+
+string Sort::getNom() const
+{
+	return m_nom;
+}
+
+int Sort::getPuissance() const
+{
+	return m_puissance;
+}
+
+BouleDeFeu::BouleDeFeu(std::string nom, int puissance, int niveauRequis) : Sort(nom, puissance, niveauRequis)
+{
+	m_ID = 0;
+}
+
+int BouleDeFeu::getID() const
+{
+	return m_ID;
+}
+
+BouleDeGlace::BouleDeGlace(std::string nom, int puissance, int niveauRequis) : Sort(nom, puissance, niveauRequis)
+{
+	m_ID = 1;
+}
+
+int BouleDeGlace::getID() const
+{
+	return m_ID;
+}
+
+BouleDeFoudre::BouleDeFoudre(std::string nom, int puissance, int niveauRequis) : Sort(nom, puissance, niveauRequis)
+{
+	m_ID = 2;
+}
+
+int BouleDeFoudre::getID() const
+{
+	return m_ID;
+}
